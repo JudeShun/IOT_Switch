6ea6d2e9dc8102819f120fa3a7897edaeb689147
nanitkatipunan <nanit.katipunan@student.passerellesnumeriques.org>
2019-10-08 21:56:05 +0800

    final

--- public/controller.js
+++ public/controller.js
@@ -1,28 +1,64 @@
 
 var client;
 var topicArray = []
-
-$('#btnOn').on('click', function () {
+$('#btnoff').on('click', function () {
 	// connect
 	var connect = "connect button clicked.."
 	console.log(connect)
 	client = mqtt.connect("wss://test.mosquitto.org:8081/mqtt")
 	client.on("connect", function () {
-		var topic = "gamboa/device/status"
-		var message = "turned on"
+		var topic = "cabalhao/fan/status"
+		var message = "Fan is turned off!"
 		client.publish(topic, message);
+		document.getElementById("message").innerHTML = "Fan is turn off";
 		console.log("Successfully connected");
-		console.log("address: " + "wss://test.mosquitto.org:8081/mqtt");
 	})
 	
 })
 
-$("#btnOff").click(function () {
-	var topic = "gamboa/device/status"
-	var message = "turned off"
-	client.publish(topic, message);
-	console.log("Disconnected!")
-})//end disconnect
+$('#btn1').on('click', function () {
+	// connect
+	var connect = "connect button clicked.."
+	console.log(connect)
+	client = mqtt.connect("wss://test.mosquitto.org:8081/mqtt")
+	client.on("connect", function () {
+		var topic = "cabalhao/fan/status"
+		var message = "Fan is turned to 1!"
+		client.publish(topic, message);
+		document.getElementById("message").innerHTML ="Fan is turned to 1!";
+		console.log("Successfully connected");
+	})
+	
+})
+
+$('#btn2').on('click', function () {
+	// connect
+	var connect = "connect button clicked.."
+	console.log(connect)
+	client = mqtt.connect("wss://test.mosquitto.org:8081/mqtt")
+	client.on("connect", function () {
+		var topic = "cabalhao/fan/status"
+		var message = "Fan is turned to 2!"
+		client.publish(topic, message);
+		document.getElementById("message").innerHTML ="Fan is turned to 2!";
+		console.log("Successfully connected");
+	})
+	
+})
+	$('#btn3').on('click', function () {
+	// connect
+	var connect = "connect button clicked.."
+	console.log(connect)
+	client = mqtt.connect("wss://test.mosquitto.org:8081/mqtt")
+	client.on("connect", function () {
+		var topic = "cabalhao/fan/status"
+		var message = "Fan is turned to 3!"
+		client.publish(topic, message);
+		document.getElementById("message").innerHTML ="Fan is turned to 3!";
+		console.log("Successfully connected");
+	})
+	
+})
 
 
 
